d37a9f72bf89d437de6bbe229231b5e2a39ad099
shoshee <shosheesharmin@gmail.com>
2021-02-08 20:28:44 +0600

    last commit

---
+++ script.js
@@ -0,0 +1,87 @@
+const searchMeals = () => {
+    const searchText = document.getElementById('search-field').value
+    // console.log(searchText); 
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`
+    // console.log(url);
+    // fetch(url)
+    //     .then(response => response.json())
+    //     .then(data => displayMeals(data.meals))
+
+    //     .catch((error) => {
+    //         alert("Invalid Meal Name. Please put a valid meal name.");
+    //     })
+
+    
+        if(searchText===''){
+            alert("Please type your desire meal first.And give your eyes ease.");
+            
+        }
+        else{
+            fetch(url)
+        .then(response => response.json())
+        .then(data => displayMeals(data.meals))
+
+        .catch((error) => {
+            alert("Invalid Meal Name. Please put a valid meal name.");
+        })
+
+        }
+        
+        
+       
+
+
+}
+
+const displayMeals = meals => {
+    // console.log(meals)
+    const mealContainer = document.getElementById('meal-container');
+    mealContainer.innerHTML = '';
+    meals.forEach(meal => {
+        const mealDiv = document.createElement("div");
+        mealDiv.className = 'meal';
+
+        const mealInfo = `
+        <div onclick = "displayMealDetail('${meal.idMeal}')">
+        <img src = "${meal.strMealThumb}">
+        <h5>${meal.strMeal}</h5>
+        </div>
+    `
+
+
+        mealDiv.innerHTML = mealInfo;
+        mealContainer.appendChild(mealDiv);
+
+    });
+
+}
+
+const displayMealDetail = mealName => {
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealName}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => renderMealInfo(data.meals));
+}
+
+const renderMealInfo = mealItems => {
+
+
+    mealItems.forEach(meal => {
+        const mealDetail = document.getElementById("meal-details")
+        mealDetail.innerHTML = `
+        <img src="${meal.strMealThumb}">
+        <h2>${meal.strMeal}</h2>
+        <p>${meal.strIngredient1}</p>
+        <p>${meal.strIngredient2}</p>
+        <p>${meal.strIngredient3}</p>
+        <p>${meal.strIngredient4}</p>
+        <p>${meal.strIngredient5}</p>
+        <p>${meal.strIngredient6}</p>
+        <p>${meal.strIngredient7}</p>
+        <p>${meal.strIngredient8}</p>
+        <p>${meal.strIngredient9}</p>
+        <p>${meal.strIngredient10}</p>
+        
+        `
+    });
+}
